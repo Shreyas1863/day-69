4a7f0b44c94c1edd0d78fce3b8129fdfbd5f4ed8
Shreyas Tare <neoshreyast@neologic.com>
2026-03-16 12:11:40 +0530

    day-69 gunicorn installed_1

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from sqlalchemy import Integer, String, Text
 from functools import wraps
 from werkzeug.security import generate_password_hash, check_password_hash
 from sqlalchemy.orm import relationship
-# Import your forms from the forms.py
+
 from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 
 '''
